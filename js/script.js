2499345ddaed8c866902be9b492b38826792f239
Kai Nguyen <159185076+kai-nguyen-4@users.noreply.github.com>
2024-05-10 18:47:48 +0000

    created division form

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Kai Nguyen All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kai Nguyen
+// Created on: May 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function divisionClicked() {
+  let numerator = parseInt(document.getElementById("num1-entered").value)
+  const denominator = parseInt(document.getElementById("num2-entered").value)
+  let counter = 0
+  let remainder = 0
+
+  while (true) {
+    if (numerator < denominator) {
+    remainder += numerator
+    break
+    } else {
+      document.getElementById("answer").innerHTML += numerator + " - " + denominator + " = " + (numerator - denominator) + "<br />"
+    }
+    numerator -= Math.abs(denominator)
+    counter++
+    console.log(numerator)
+  }
+  console.log(numerator)
+  document.getElementById("answer").innerHTML += "The answer is = " + counter + " R" + remainder
+}
+
